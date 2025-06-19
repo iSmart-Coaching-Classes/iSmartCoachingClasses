da715a48e5272768f6baceddc81c1f3fdfce9409
iSmart-Coaching-Classes <ismartcoachingclasses@gmail.com>
2025-06-19 21:30:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const menuOpenButton = document.querySelector ("#menu-open-button");
 const menuCloseButton = document.querySelector ("#menu-close-button");
 const navLinks = document.querySelectorAll (".nav-menu .nav-link");
-const form = document.querySelector('form');
-const fullName = document.getElementsById("Name");
-const email = document.getElementsById("email");
-const phone = document.getElementsById("phone");
-const message = document.getElementsById("message");
 
 menuOpenButton.addEventListener("click", () => {
     //Toggle Mobile menu visibility
@@ -19,24 +14,29 @@ menuCloseButton.addEventListener("click", () => menuOpenButton.click ());
 navLinks.forEach(link => {
     link.addEventListener("click", () => menuOpenButton.click ());
 })
-
-function sendEmail() {
-    const bodyMessage = `Full Name: ${Name.value} <br> Email: ${email.value} <br> Phone Number: ${phone.value} <br> Message: ${message.value}`; 
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "ismartcoachingclasses@gmail.com",
-        Password : "1C031B1C1A9EF49CE0405F2F059FF8D06F37",
-        To : 'ismartcoachingclasses@gmail.com',
-        From : "ismartcoachingclasses@gmail.com",
-        Subject : "This is the subject",
-        Body : bodyMessage
-    }).then(
-      message => alert(message)
-    );
+const slides = document.querySelectorAll(".islide")
+var counter = 0;
+slides.forEach(
+  (islide, index) =>{
+    islide.style.left = `${index * 100}%`
+  }
+)
+const goPrev = () =>{
+  if(counter > 0){
+    counter--
+  slideImage()
+  }
 }
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    sendEmail();
-});
+const goNext = () =>{
+  if(counter < slides.length-1){
+    counter++
+  slideImage()
+  }
+}
+const slideImage = () => {
+  slides.forEach(
+    (islide) => {
+      islide.style.transform = `translateX(-${counter * 100}%)`
+    }
+  )
+}
